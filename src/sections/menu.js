5eaa79ab2ea71982222b35830c5de4945b222296
lol123ivan777 <idiidmmme@gmail.com>
2025-12-12 12:00:49 +0300

    Update menu.js

--- src/sections/menu.js
+++ src/sections/menu.js
@@ -1,16 +1,30 @@
-const { mainMenu } = require("../navigation");
-
-function showHome(bot, msg) {
-  const text =
-    "🎪 *Цирк Никулина на Цветном бульваре*\n\n" +
-    "Выберите раздел 👇";
-
-  bot.editMessageText(text, {
-    chat_id: msg.chat.id,
-    message_id: msg.message_id,
-    parse_mode: "Markdown",
-    reply_markup: mainMenu()
-  });
-}
-
-module.exports = { showHome };
+exports.showHome = (bot, msg) => {
+  bot.editMessageText(
+    "🎪 *Цирк Никулина на Цветном бульваре*\n\nВыберите раздел 👇",
+    {
+      chat_id: msg.chat.id,
+      message_id: msg.message_id,
+      parse_mode: "Markdown",
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: "📰 Новости", callback_data: "news" },
+            { text: "🎭 Расписание", callback_data: "schedule" }
+          ],
+          [
+            { text: "🤹 Артисты", callback_data: "artists" },
+            { text: "📍 Контакты", callback_data: "contacts" }
+          ],
+          [
+            {
+              text: "🎟 БИЛЕТЫ / ПРИЛОЖЕНИЕ",
+              web_app: {
+                url: "https://ТВОЙ-URL.vercel.app"
+              }
+            }
+          ]
+        ]
+      }
+    }
+  );
+};
