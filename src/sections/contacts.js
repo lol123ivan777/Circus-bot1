30f28c4d531554563eef9be0784f2c1623bef786
lol123ivan777 <idiidmmme@gmail.com>
2025-12-12 11:36:50 +0300

    Update contacts.js

--- src/sections/contacts.js
+++ src/sections/contacts.js
@@ -1,19 +1,29 @@
 function showContacts(bot, msg) {
   const text =
-    "📍 *Контакты*\n\n" +
-    "*Адрес:*\nМосква, Цветной бульвар, 13\n\n" +
-    "*Телефоны:*\n" +
-    "+7 (495) 628-83-49\n" +
+    "📍 Контакты\n\n" +
+    "Адрес:\n" +
+    "127051, Россия, Москва, Цветной бульвар, 13\n\n" +
+    "Телефон:\n" +
+    "+7 (495) 628-83-49\n\n" +
+    "Администрация:\n" +
     "+7 (495) 780-31-35\n\n" +
-    "*Соцсети:*\n" +
+    "Социальные сети:\n" +
     "VK: https://vk.com/circusnikulin\n" +
-    "TG: https://t.me/nikulin_circus\n\n" +
-    "*Почта:*\ninfo@circusnikulin.ru\npr@circusnikulin.ru";
+    "Telegram: https://t.me/nikulin_circus\n\n" +
+    "Отдел забытых вещей:\n" +
+    "Забытые вещи можно забрать во время работы гардероба в дни представлений.\n\n" +
+    "Электронные билеты:\n" +
+    "https://circusnikulin.ru/tickets\n\n" +
+    "Поддержка электронных билетов:\n" +
+    "8 (495) 215-0000\n" +
+    "ticket@profticket.ru\n\n" +
+    "Электронная почта:\n" +
+    "info@circusnikulin.ru\n" +
+    "pr@circusnikulin.ru";
 
   bot.editMessageText(text, {
     chat_id: msg.chat.id,
     message_id: msg.message_id,
-    parse_mode: "Markdown",
     reply_markup: {
       inline_keyboard: [[{ text: "🏠 На главную", callback_data: "home" }]]
     }
