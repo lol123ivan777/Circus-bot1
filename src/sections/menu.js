056f1f808890072b99bc574511b9779e1c83fe2a
lol123ivan777 <idiidmmme@gmail.com>
2025-12-12 12:15:08 +0300

    Update menu.js

--- src/sections/menu.js
+++ src/sections/menu.js
@@ -1,31 +1,20 @@
-exports.showHome = (bot, msg) => {
-  bot.editMessageText(
-    "🎪 *Цирк Никулина на Цветном бульваре*\n\nВыберите раздел 👇",
-    {
-      chat_id: msg.chat.id,
-      message_id: msg.message_id,
-      parse_mode: "Markdown",
-      reply_markup: {
-        inline_keyboard: [
-          [
-            { text: "📰 Новости", callback_data: "news" },
-            { text: "🎭 Расписание", callback_data: "schedule" }
-          ],
-          [
-            { text: "🤹 Артисты", callback_data: "artists" },
-            { text: "📍 Контакты", callback_data: "contacts" }
-          ],
-            [
-  {
-    text: "🎟 БИЛЕТЫ / ПРИЛОЖЕНИЕ",
-    web_app: { url: "https://circus-nikulin-app.vercel.app/" }
-  }
-]
-              }
-            }
-          ]
-        ]
+reply_markup: {
+  inline_keyboard: [
+    [
+      { text: "📰 Новости", callback_data: "news" },
+      { text: "🎭 Расписание", callback_data: "schedule" }
+    ],
+    [
+      { text: "🤹‍♂️ Артисты", callback_data: "artists" },
+      { text: "📍 Контакты", callback_data: "contacts" }
+    ],
+    [
+      {
+        text: "🎟 БИЛЕТЫ / ПРИЛОЖЕНИЕ",
+        web_app: {
+          url: "https://circus-nikulin-app.vercel.app/"
+        }
       }
-    }
-  );
-};
+    ]
+  ]
+}
