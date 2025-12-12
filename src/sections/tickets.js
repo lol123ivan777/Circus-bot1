07d118c6b218490239269592fc7474ec74c07c60
lol123ivan777 <idiidmmme@gmail.com>
2025-12-12 11:16:00 +0300

    Update tickets.js

--- src/sections/tickets.js
+++ src/sections/tickets.js
@@ -1,15 +1,16 @@
-const { backHome } = require("../navigation");
-
 function showTickets(bot, msg) {
   const text =
     "🎟 *Билеты*\n\n" +
-    "Приобрести билеты можно на официальном сайте цирка.";
+    "Приобрести билеты можно на официальном сайте:\n" +
+    "https://circusnikulin.ru/tickets";
 
   bot.editMessageText(text, {
     chat_id: msg.chat.id,
     message_id: msg.message_id,
     parse_mode: "Markdown",
-    reply_markup: backHome()
+    reply_markup: {
+      inline_keyboard: [[{ text: "🏠 На главную", callback_data: "home" }]]
+    }
   });
 }
 
